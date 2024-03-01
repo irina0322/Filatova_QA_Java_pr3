07bc939ea90ead77c1cefd2c835599c6cfbe4d36
Irina <irina.filatova@atlasteam.me>
2024-03-01 22:14:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int number : array) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+    }
+}
